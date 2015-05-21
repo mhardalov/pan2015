0fadc42851d1892baf96283b43794b49fc04c90c
Momchil Hardalov <momchil.hardalov@gmail.com>
2015-05-21 12:00:21 +0300

    Clear the annotations for document

--- src/main/resources/groovy/add-annotations.groovy
+++ src/main/resources/groovy/add-annotations.groovy
@@ -3,3 +3,15 @@ long start = 0l
 long end = text.length() - 1l
 FeatureMap map = Factory.newFeatureMap()
 outputAS.add (start, end, "person", map)
+
+FeatureMap df = doc.getFeatures();	
+df["o"] = "";
+df["c"] = "";
+df["e"] = "";
+df["a"] = "";
+df["s"] = "";
+df["age"] = "";
+df["sex"] = "";
+df["id"] = "";
+df["lang"] = "";
+
